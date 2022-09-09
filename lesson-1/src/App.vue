4deda84f84fa01fe99a599b86fddb8c79cc25338
TranTam <tamvaa1306@gmail.com>
2022-09-09 11:06:08 +0700

    types interfaces

--- lesson-1/src/App.vue
+++ lesson-1/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="app">
-    <h1>Hello, {{ name }}'s here! I'm {{ age }} years old.</h1>
+    <!-- <h1>Hello, {{ name }}'s here! I'm {{ age }} years old.</h1>
     <button v-on:click="changeName('Jack')">Change Name</button>
-    <button v-on:click="changeAge(30)">Change Age</button>
+    <button v-on:click="changeAge(30)">Change Age</button> -->
+    <p>{{ jobs[0] }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs, ref } from "vue";
+
+// types
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
@@ -20,19 +24,25 @@ export default defineComponent({
     // return { ...toRefs(state) };
 
     // Ref
-    const name = ref<string>("Owen");
-    const age = ref<number | string>(25);
-    return { name, age };
+    // const name = ref<string>("Owen");
+    // const age = ref<number | string>(25);
+    // return { name, age };
+
+    const jobs = ref<Job[]>([
+      { title: "Farmer", location: "Farm", salary: 30000, id: "1" },
+      { title: "Doctor", location: "Hospital", salary: 60000, id: "2" },
+    ]);
+    return { jobs };
   },
   methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: string | number) {
-      this.age = age;
-      return age;
-    },
+    // changeName(name: string) {
+    //   this.name = name;
+    //   return name;
+    // },
+    // changeAge(age: string | number) {
+    //   this.age = age;
+    //   return age;
+    // },
   },
 });
 </script>
